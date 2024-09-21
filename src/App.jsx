fcc16e4e788ecb4b9717ecd8b531208eb54ceff4
avinash23em <ap23vi@gmail.com>
2024-09-21 17:50:30 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import './App.css'
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StartRating from './components/star-rating';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div className='App'>
-    <RandomColor/>
+    
+    <StartRating noofstars={10}/>
    </div>
   );
 }

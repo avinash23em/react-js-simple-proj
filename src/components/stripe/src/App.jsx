60af20c559706f76129af01d6c29b3082c428a92
avinash23em <ap23vi@gmail.com>
2025-02-28 01:27:23 +0530

    intial

--- src/components/stripe/src/App.jsx
+++ src/components/stripe/src/App.jsx
@@ -8,7 +8,20 @@ function App() {
 
   return (
     <>
-      <div className='text-3xl bg-blue-500 font-bold'>stripe page</div>
+      <div className='text-3xl bg-blue-500 font-bold grid grid-cols-8 px-8 py-4'>
+      <div className='col-span-1'></div>
+      <div className='col-span-4 flex justify-between items-center '>
+        <div >stripe</div>
+        <div >products</div>
+        <div >solutions</div>
+        <div >resources</div>
+      </div>
+      <div className='col-span-1'></div>
+      <div className='col-span-2 flex justify-start items-center gap-6'>
+      <div >signin</div>
+      <div >contact</div>
+      </div>
+      </div>
         </>
   )
 }

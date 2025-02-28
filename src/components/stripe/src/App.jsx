73ab24b3ae157625f7f468c6e57ec76009d7aa84
avinash23em <ap23vi@gmail.com>
2025-02-28 18:20:47 +0530

    intial

--- src/components/stripe/src/App.jsx
+++ src/components/stripe/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Logo from '../components/icons/Logo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-3xl bg-blue-500 font-bold grid grid-cols-8 px-8 py-4'>
+      <div className='text-xl bg-blue-500 font-bold grid grid-cols-8 px-8 py-4'>
       <div className='col-span-1'></div>
       <div className='col-span-4 flex justify-between items-center '>
-        <div >stripe</div>
+        <div className='h-100 w-200' ><Logo/></div>
         <div >products</div>
         <div >solutions</div>
         <div >resources</div>

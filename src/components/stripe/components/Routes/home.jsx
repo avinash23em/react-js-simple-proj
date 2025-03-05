4950b1f1d76c6f1bc53cc291c75719e9adddd49f
avinash23em <ap23vi@gmail.com>
2025-03-05 19:25:06 +0530

    intial

---
+++ src/components/stripe/components/Routes/home.jsx
@@ -0,0 +1,7 @@
+
+export default function Home() {
+  return (
+    <div className="bg-pink-500">what do u mean</div>
+  )
+}
+
--- src/components/stripe/src/App.jsx
+++ src/components/stripe/src/App.jsx
@@ -5,58 +5,14 @@ import './App.css'
 import Logo from '../components/icons/Logo'
 import Arrow1 from '../components/icons/arrow'
 import Arrow from '../components/icons/arrow1'
+import Home from '../components/Routes/home'
+import Navabr from '../components/Routes/navbar'
 
 export default function App() {
   return (
-    <div className='text-xl bg-blue-500 font-bold grid grid-cols-8 px-8 py-4'>
-    
-      <div className='col-span-1'></div>
-
-      
-      <div className='col-span-4 flex justify-between items-center'>
-        <div className='h-100 w-200'>
-          <Logo />
-        </div>
-
-        <div>
-          <button className="flex items-center gap-1">
-            Products
-            <span className="flex items-center">
-    <Arrow />
-    <Arrow1 />
-  </span>
-          </button>
-        </div>
-
-        <div>
-          <button className="flex items-center gap-1">
-            Solutions
-            <span className='flex'>
-    <Arrow />
-    <Arrow1 />
-  </span>
-          </button>
-        </div>
-
-        <div>
-          <button className="flex items-center gap-1">
-            Resources
-            <span className='flex'>
-    <Arrow />
-    <Arrow1 />
-  </span>
-          </button>
-        </div>
-      </div>
-
-      
-      <div className='col-span-1'></div>
-
-      
-      <div className='col-span-2 flex justify-start items-center gap-6'>
-        <div>Sign in</div>
-        <div>Contact</div>
-      </div>
+    <div className="bg-pink-400">
+    <div><Navabr/></div>
+      <div className='h-screen'><Home/></div>
     </div>
   );
 }

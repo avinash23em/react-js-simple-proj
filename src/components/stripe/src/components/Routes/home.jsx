48346e48a80598f5ce2dcec4dbf4f45367f3d9b7
avinash23em <ap23vi@gmail.com>
2025-03-14 20:46:35 +0530

    intial

--- src/components/stripe/src/components/Routes/home.jsx
+++ src/components/stripe/src/components/Routes/home.jsx
@@ -2,11 +2,26 @@ import Start from "../icons/startbg";
 
 export default function Home() {
   return (
-    <div>
-    <div><Start/>
-    <h1>transform the payments</h1>
+    <div className="relative w-full h-screen flex items-center justify-center">
+      
+      <div className="absolute inset-0">
+        <Start className="w-full h-full object-cover" />
+      </div>
+
+      
+      <div className="relative z-10 text-white text-3xl font-bold flex justify-center items-center">
+      <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
+
 

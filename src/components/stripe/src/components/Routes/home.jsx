0cda1c8ef27c77523aa9a280c6dcd25b64b53e89
avinash23em <ap23vi@gmail.com>
2025-03-21 23:00:41 +0530

    intial

--- src/components/stripe/src/components/Routes/home.jsx
+++ src/components/stripe/src/components/Routes/home.jsx
@@ -1,28 +1,37 @@
+import Phone from "../icons/phone";
 import Start from "../icons/startbg";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen flex items-center justify-center">
+    <div className="relative w-full h-screen">
       
-      <div className="absolute inset-0">
-        <Start className="w-full h-full object-contain " />
+      <div className="absolute inset-0 -z-10">
+        <Start className="w-full h-full object-cover" />
       </div>
 
       
-      <div className="relative z-10 text-white text-3xl font-bold flex justify-center items-center">
-      <h1 className="font-bold text-black pl-6 pb-6">
+      <div className="grid grid-cols-5 h-full">
+        
+        <div className="col-span-1"></div>
+
+        
+        <div className="col-span-2 flex flex-col justify-center pl-6">
+          <h1 className="font-bold text-black text-4xl pb-6">
             Payments infrastructure <br />
             for the internet
           </h1>
-          <p>
+          <p className="text-lg text-gray-700">
             Millions of companies of all sizes—from startups to Fortune 500s—use
-            <br/>
             Stripe’s software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
+        </div>
+
+        
+        <div className="col-span-2 flex items-center justify-center">
+          <Phone  />
+        </div>
       </div>
     </div>
   );
 }
-
-
